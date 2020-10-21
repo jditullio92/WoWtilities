1d81c1a67829f18b8687cd6685d84bc3ddcc7e23
John DiTullio <john@norfielddp.com>
2020-10-21 12:16:18 -0400

    initialized npm

--- worldbuffs.js
+++ worldbuffs.js
@@ -1,48 +1,41 @@
 // Author: John DiTullio aka gankaskhan
-
-/*
-EXAMPLE NWB STRING:
-Rend: 1 hour 53 minutes. (11:51am server time)
-Onyxia: 4 hours 34 minutes. (2:32pm server time)
-Nefarian: 6 hours 19 minutes. (4:16pm server time)
-*/
-
 // Page load event handler
 $(document).ready(function () {
+    // set timers (timeout prevents error when doing replace on string)
+    function pasteeventhandler() { setTimeout(function () { pasteEventHandler(); }, 1); }
     // text area paste/change event handler
-    $('#timerTextArea').on("paste change", function () {
-        // set timers (timeout prevents error when doing replace on string)
-        setTimeout(function () { pasteEventHandler(); }, 1);
-    });
+    $('#timerTextArea').on("paste change", pasteeventhandler);
 });
 
 // Handle parsing NovaWorldBuff string
 function pasteEventHandler() {
     // Get string as array
-    var timers = $('#timerTextArea').val().split("\n");
+    timers = $('#timerTextArea').val().split("\n");
+
+    for (timer of timers) {
+        
+    }
+
 
     // Split each buff time into a string that can be used to parse a datetime object
     var rendTimeText = timers[0].split("(")[1].split(' ')[0];
     rendTimeText = getAmPmDateFriendly(rendTimeText);
-    rendTime = new Date(getDateAsString() + " " + rendTimeText + " PST");
-    
+    rendTime = new Date(getDateAsString() + " " + rendTimeText + " PDT");
+
     // Update text for Rend
     $('#timerRend').val(getFormattedDateTimeAmPm(rendTime));
 
     var onyTimeText = timers[1].split("(")[1].split(' ')[0];
     onyTimeText = getAmPmDateFriendly(onyTimeText);
-    onyTimeText = new Date(getDateAsString() + " " + onyTimeText + " PST");
-    
+    onyTimeText = new Date(getDateAsString() + " " + onyTimeText + " PDT");
+
     // Update text for Onyxia
     $('#timerOny').val(onyTimeText);
 
     var nefTimeText = timers[2].split("(")[1].split(' ')[0];
     nefTimeText = getAmPmDateFriendly(nefTimeText);
-    console.log("NOT SPECIFIED: " + new Date(getDateAsString() + " " + nefTimeText));
-    console.log("PST SPECIFIED: " + new Date(getDateAsString() + " " + nefTimeText+ " PST"));
-    
-    nefTimeText = new Date(getDateAsString() + " " + nefTimeText + " PST");
-    
+    nefTimeText = new Date(getDateAsString() + " " + nefTimeText + " PDT");
+
     // Update text for Nefarian
     $('#timerNef').val(nefTimeText);
 }
@@ -58,7 +51,12 @@ function getAmPmDateFriendly(str) { return str.replace("am", " am").replace("pm"
 
 //#endregion
 
-// dev codes
+/*
+EXAMPLE NWB STRING:
+Rend: 2 hours 9 minutes. (1:00pm server time)
+Onyxia: 2 hours 39 minutes. (1:31pm server time)
+Nefarian: 5 hours 3 minutes. (3:54pm server time)
+*/
 function test() {
     $('#timerTextArea').val("Rend: 1 hour 53 minutes. (11:51am server time)\r\nOnyxia: 4 hours 34 minutes. (2:32pm server time)\r\nNefarian: 6 hours 19 minutes. (4:16pm server time)");
 }
