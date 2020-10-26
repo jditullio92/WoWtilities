89992182488826d9ec1ff9120f2fac12376f90b5
John DiTullio <john@norfielddp.com>
2020-10-26 17:25:02 -0400

    Removed file

--- public/worldbuffs/time.js
+++
@@ -1,6 +0,0 @@
-// import moment from 'moment';
-var moment = require('moment');
-
-function getNow() { return moment().format(); }
-
-function convertToMomentDate(date) { return moment(date).format(); }
